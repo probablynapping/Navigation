b0220e877e1b606b2b910c65f8fe2a217d2b4ab4
Diana <diudiana7788@gmail.com>
2025-07-24 10:12:08 -0700

    added navigation stack, 2nd nav view, and some text to label the views

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack{
+                Text("This is the root view")
+                NavigationLink(destination: Text("You have arrived at the second view!")) {
+                    Text("Click Me!")
+                }
+            }
         }
-        .padding()
     }
 }
 
